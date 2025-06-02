47b4a4fa6036439429e2a94821c78d4fc5f8838b
joel9579 <23bsds225joelkingsleyg@skacas.ac.in>
2025-06-02 20:38:15 +0530

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,6 @@ export default defineConfig({
     outDir: 'dist',
   },
   server: {
-    proxy: {
-      '/api' : 'https://quantum-coin-ai.vercel.app/',
-    }
     port: 3000,
     historyApiFallback: true
   }
